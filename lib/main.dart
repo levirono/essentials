6c96ec47018e779fa5a5e7b0902d44b9f78f5538
levirono <ronolawi3@gmail.com>
2025-07-07 18:32:08 +0300

    added receive sharing intent

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,51 @@
 import 'package:flutter/material.dart';
 import 'UI/home.dart';
+import 'features/pdf_reader.dart';
+import 'dart:async';
+import 'package:receive_sharing_intent/receive_sharing_intent.dart';
+import 'dart:io';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String? _sharedPdfPath;
+  StreamSubscription? _intentDataStreamSubscription;
+
+  @override
+  void initState() {
+    super.initState();
+    // For app in memory
+    _intentDataStreamSubscription = ReceiveSharingIntent.getMediaStream().listen((List<SharedMediaFile> value) {
+      if (value.isNotEmpty && value[0].path.toLowerCase().endsWith('.pdf')) {
+        setState(() {
+          _sharedPdfPath = value[0].path;
+        });
+      }
+    }, onError: (err) {});
+    // For app launch
+    ReceiveSharingIntent.getInitialMedia().then((List<SharedMediaFile> value) {
+      if (value.isNotEmpty && value[0].path.toLowerCase().endsWith('.pdf')) {
+        setState(() {
+          _sharedPdfPath = value[0].path;
+        });
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _intentDataStreamSubscription?.cancel();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +55,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: HomePage(), // Directly show HomePage
+      home: _sharedPdfPath != null ? PdfReaderPage(filePath: _sharedPdfPath!) : HomePage(),
     );
   }
 }
