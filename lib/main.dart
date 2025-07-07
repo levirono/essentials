955eb704ecd21192574859369576d17b6a420451
levirono <ronolawi3@gmail.com>
2025-07-07 18:49:06 +0300

    updated receicve share intent

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,7 @@ import 'dart:async';
 import 'package:receive_sharing_intent/receive_sharing_intent.dart';
 import 'dart:io';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
@@ -17,33 +15,42 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  StreamSubscription? _intentSub;
   String? _sharedPdfPath;
-  StreamSubscription? _intentDataStreamSubscription;
 
   @override
   void initState() {
     super.initState();
-    // For app in memory
-    _intentDataStreamSubscription = ReceiveSharingIntent.getMediaStream().listen((List<SharedMediaFile> value) {
+    // Listen for shared files while app is in memory
+    _intentSub = ReceiveSharingIntent.instance.getMediaStream().listen(
+      (List<SharedMediaFile> value) {
+        if (value.isNotEmpty && value[0].path.toLowerCase().endsWith('.pdf')) {
+          setState(() {
+            _sharedPdfPath = value[0].path;
+          });
+        }
+      },
+      onError: (err) {
+        // Handle error if needed
+      },
+    );
+    // Listen for shared files when app is launched
+    ReceiveSharingIntent.instance.getInitialMedia().then((
+      List<SharedMediaFile> value,
+    ) {
       if (value.isNotEmpty && value[0].path.toLowerCase().endsWith('.pdf')) {
         setState(() {
           _sharedPdfPath = value[0].path;
         });
       }
-    }, onError: (err) {});
-    // For app launch
-    ReceiveSharingIntent.getInitialMedia().then((List<SharedMediaFile> value) {
-      if (value.isNotEmpty && value[0].path.toLowerCase().endsWith('.pdf')) {
-        setState(() {
-          _sharedPdfPath = value[0].path;
-        });
-      }
+      // Reset after processing
+      ReceiveSharingIntent.instance.reset();
     });
   }
 
   @override
   void dispose() {
-    _intentDataStreamSubscription?.cancel();
+    _intentSub?.cancel();
     super.dispose();
   }
 
@@ -55,7 +62,10 @@ class _MyAppState extends State<MyApp> {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: _sharedPdfPath != null ? PdfReaderPage(filePath: _sharedPdfPath!) : HomePage(),
+      home:
+          _sharedPdfPath != null
+              ? PdfReaderPage(filePath: _sharedPdfPath!)
+              : HomePage(),
     );
   }
 }
